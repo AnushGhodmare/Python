6c5395c9e3090e1b2a1f337e12df87b93e421524
anushghodmare <anushghodmare@gmail.com>
2024-10-02 16:24:45 +0530

    THIS IS MY FIRST GITHUB CODE

---
+++ anush.py
@@ -0,0 +1,11 @@
+n1=int(input("ENTER THE FIRST NUMBER"))
+n2=int(input("ENTER THE SECOND NUMBER"))
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1/n2
+print ("ADDITION=",add)
+print ("SUBSTRACTION=",sub)
+print ("MULTIPLICATION=",mul)
+print ("DIVISION=",div)
+
